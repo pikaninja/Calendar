4dbf8055abe5ee96497eae76ee097c7926a3a4a8
cyrus01337 <cyrus21337@gmail.com>
2021-02-07 12:16:32 +0000

    Load Jishaku

--- dependencies/bot.py
+++ dependencies/bot.py
@@ -11,14 +11,17 @@ class CalendarBot(commands.Bot):
         self._uptime = datetime.datetime.utcnow()
         self.db = database.create(user="postgres", password="root")
 
+        print("Calendar Bot is starting!")
         super().__init__(*args, **kwargs)
+
+        self.load_extension("jishaku")
 
     @property
     async def uptime(self):
         return humanize.naturaldelta(datetime.datetime.utcnow() - self._uptime)
 
     async def on_ready(self):
-        print(f"Calendar Bot is starting!\nLogged in as: {self.user}")
+        print(f"Logged in as: {self.user}")
 
     async def close(self):
         await self.db.close()
