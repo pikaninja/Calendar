cefa31279896fcfbf45411c4b334031ff4b34ddd
cyrus01337 <cyrus21337@gmail.com>
2021-02-13 19:30:00 +0000

    Add group functionality

--- dependencies/database.py
+++ dependencies/database.py
@@ -1,12 +1,13 @@
 import asyncio
 import datetime
 import functools
-from typing import Coroutine, Dict, List, Optional, Tuple
+from typing import Coroutine, Dict, List, Optional, Set, Tuple
 
 import asyncpg
 from asyncpg.pool import Pool
 
-from .objects import Context, MemberProxy, Reminder
+from .objects import MemberProxy, Reminder
+from utils.errors import DuplicateGroup
 
 
 class Database:
@@ -26,7 +27,7 @@ class Database:
         self.pool: Pool = None
         self.groups: Dict[int, Tuple[int]] = []
         self.members: Dict[int, int] = {}
-        self.reminders: Dict[int, List[Reminder]] = {}
+        self.reminders: Dict[int, List[Reminder]] = {"_groups": {}}
         self.loop = asyncio.get_event_loop()
 
         self.loop.create_task(self.__ainit__())
@@ -49,8 +50,8 @@ class Database:
 
                 CREATE TABLE IF NOT EXISTS reminders(
                     id SERIAL PRIMARY KEY,
+                    member_id BIGINT,
                     group_id BIGINT,
-                    member_id BIGINT NOT NULL,
                     timestamp TIMESTAMP NOT NULL,
                     event VARCHAR(1000)
                 );
@@ -61,29 +62,50 @@ class Database:
             reminders = await conn.fetch("SELECT * FROM reminders;")
 
             for _id, member_ids in groups:
-                self.groups[_id] = member_ids
+                self.groups[_id] = set(member_ids)
 
             for _id, member_id in members:
-                groups = self.get_groups(member_id)
-                self.members[member_id] = MemberProxy(_id, groups)
+                groups = self.find_groups(member_id)
+                self.members[member_id] = MemberProxy(_id, member_id, groups)
 
-            for _id, member_id, *args in reminders:
-                members_reminders = self.reminders.setdefault(member_id, [])
-                index = len(members_reminders)
+            for _id, member_id, group_id, *args in reminders:
+                select_id = member_id
+                which = self.reminders
+
+                if not member_id:
+                    select_id = group_id
+                    which = self.reminders["_groups"]
+                reminders_obj = which.setdefault(select_id, [])
+                index = len(reminders_obj)
                 obj = Reminder(_id, index, *args)
 
-                members_reminders.append(obj)
+                reminders_obj.append(obj)
         self._loaded.set()
 
-    def _get_member_reminders(self, member_id: int):
-        return self.reminders.setdefault(member_id, {})
+    def _get_next_index(self,
+                        *, member_id: Optional[int] = None,
+                        group_id: Optional[int]):
+        if member_id:
+            return len(self.reminders.setdefault(member_id, {}))
+        elif group_id:
+            group_reminders = self.reminders["_group"]
 
-    def get_groups(self, member_id: int):
-        groups = []
+            return len(group_reminders.setdefault(group_id, {}))
+        raise ValueError("no ID passed into relevant kwargs")
 
-        for group_id, group_members_ids in self.groups.items():
+    def _invalidate_group(self, *member_ids):
+        return False
+
+    def _resolve_group_id(self, _id: int):
+        group_reminders = self.reminders["_group"]
+        return group_reminders.get(_id, None)
+
+    def find_groups(self, member_id: int):
+        groups = {}
+
+        for _id, group_members_ids in self.groups.items():
             if member_id in group_members_ids:
-                groups.append(group_id)
+                groups[_id] = set(group_members_ids)
         return groups
 
     async def wait_until_loaded(self):
@@ -93,49 +115,59 @@ class Database:
         await self.pool.close()
 
     @connect
-    async def _get_member_serial(self, conn, member_id: int):
-        serial_found = self.members.get(member_id, None)
+    async def _get_internal_id(self, conn, member_id: int):
+        internal_id = self.members.get(member_id, None)
 
-        if not serial_found:
-            serial_found = self.members[member_id] = await conn.fetchval(
+        if not internal_id:
+            internal_id = self.members[member_id] = await conn.fetchval(
                 "INSERT member_id INTO members VALUES($1) RETURNING id;",
                 member_id
             )
-        return serial_found
+        return internal_id
 
     @connect
     async def create_group(self, conn, *member_ids):
-        proxies = []
+        # (L82) requires duplicate validation
+        existing_group = self._invalidate_group(*member_ids)
 
-        for _id in member_ids:
-            proxy = await self._get_member_proxy(_id)
-            proxies.append(proxy)
+        if existing_group:
+            raise DuplicateGroup(existing_group)
+        values = []
 
-        for proxy in proxies:
-            pass
+        for i in range(1, len(member_ids) + 1):
+            values = f"${i}"
+        joined = (", ").join(values)
+
+        _id = await conn.execute(
+            f"INSERT INTO groups VALUES({joined}) RETURNING id;",
+            *member_ids
+        )
+        self.groups[_id] = member_ids
 
     @connect
     async def add_reminder(self,
                            conn,
-                           member_id: int,
+                           select_id: int,
                            timestamp: datetime.datetime,
                            event: Optional[str] = None):
-        reminders = self._get_member_reminders(member_id)
-        index = len(reminders)
-        member_serial = await self._get_member_serial(member_id)
+        id_param = "member_id"
+        which = self.reminders
+        group_found = self._resolve_group_id(select_id)
 
-        _id = await conn.execute(
-            """
-                INSERT INTO reminders(member_id, timestamp, event)
-                VALUES($1, $2, $3) RETURNING id;
-            """,
-            member_serial,
-            timestamp,
-            event
-        )
-        obj = Reminder(_id, index, timestamp, event)
+        if group_found:
+            id_param = "group_id"
+            which = group_found
+        args = [select_id, timestamp, event]
+        query = f"""
+            INSERT INTO reminders({id_param}, timestamp, event)
+            VALUES($1, $2, $3) RETURNING id;
+        """
+        kwargs = {id_param: select_id}
+        index = self._get_next_index(**kwargs)
+        _id = await conn.execute(query, *args)
+        obj = Reminder(_id, index, timestamp, event, group=group_found)
 
-        reminders.append(obj)
+        which.append(obj)
 
     @connect
     async def pop_reminder(self, conn, member_id: int, index: int):
--- dependencies/objects.py
+++ dependencies/objects.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import Optional, List
+from typing import Dict, Iterable, Optional
 
 
 class Reminder:
@@ -7,25 +7,43 @@ class Reminder:
                  _id: int,
                  index: int,
                  timestamp: datetime.datetime,
-                 event: Optional[str] = ""):
+                 event: Optional[str] = "",
+                 *, group: Optional[Dict]):
         self.id = _id
         self.index = index
         self.timestamp = timestamp
         self.event = event
+        self.group = group
 
     @property
     def late(self):
         return datetime.datetime.now() > self.timestamp
 
     @property
-    def diff(self):
+    def delta(self):
         return self.timestamp - datetime.datetime.now()
+
+    @property
+    def diff(self):
+        return self.delta.total_seconds()
+
+    @property
+    def member_ids(self):
+        if not self.group:
+            return None
+        ret = set()
+
+        for ids in self.group.values():
+            for _id in ids:
+                ret.add(_id)
+        return ret
 
 
 class MemberProxy:
-    def __init__(self, _id: int, groups: Optional[List[int]]):
+    def __init__(self, row_id: int, _id: int, groups: Optional[Dict[int, Iterable[int]]]):
+        self.row_id = row_id
         self.id = _id
-        self.groups: Optional[List[int]] = None
+        self.groups = None
 
         if groups:
             self.groups = groups
