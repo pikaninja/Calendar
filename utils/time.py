21be629dad5cbd289461767bbf3392749d9b022c
pikaninja <mileshua06@gmail.com>
2021-02-22 15:52:31 -0800

    placeholder converter

--- utils/time.py
+++ utils/time.py
@@ -13,3 +13,24 @@ class TimeConverter(commands.Converter):
             raise TimeNotParsed(fmt)
 
         return ret
+
+class PlaceholderConverter(commands.Converter):
+    async def convert(self, ctx, arg):
+        arg = arg.lower()
+        arg = arg.strip("to")
+        if " to " in arg and not arg.startswith("to"):
+            arg = arg.strip("me inat")
+            time, reminder = arg.split(" to ")
+            time = dateparser.parse(time.strip())
+            return time, reminder.strip()
+        arg = arg.replace(" in ", " at ")
+        if "at" in arg:
+            reminder, time = arg.split(" at ")
+            time = dateparser.parse(time.strip())
+            return time, reminder.strip()
+        trying = arg.split()
+        for i in range(len(trying)):
+            reminder, time = " ".join(trying[:i]), " ".join(trying[i:])
+            print(reminder, "split",  time)
+            if time := dateparser.parse(time):
+                return time, reminder
